23d4150cf30b00d7c68e178eced645c062458f23
Serban Patriciu <serban_patriciu@hotmail.com>
2022-04-30 22:49:04 +0300

    Setup the color background depending on the magnitude of the earthquake.

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -13,6 +13,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import android.graphics.drawable.GradientDrawable;
+
+import androidx.core.content.ContextCompat;
+
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
     private static final String LOG_TAG = EarthquakeAdapter.class.getSimpleName();
@@ -40,6 +44,16 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         // Get the magnitude and set the text on the TextView
         String formattedMagnitude = formatMagnitude(currentEarthquake.getMagnitude());
         magnitudeView.setText(formattedMagnitude);
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
 
         String originalLocation = currentEarthquake.getLocation();
 
@@ -106,5 +120,27 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private String formatMagnitude(double magnitude) {
         DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
         return magnitudeFormat.format(magnitude);
+    }
+
+    /**
+     * Return the color value associated with a magnitude
+     */
+    private int getMagnitudeColor(double magnitude) {
+        int colorResourceID;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1: colorResourceID = R.color.magnitude1; break;
+            case 2: colorResourceID = R.color.magnitude2; break;
+            case 3: colorResourceID = R.color.magnitude3; break;
+            case 4: colorResourceID = R.color.magnitude4; break;
+            case 5: colorResourceID = R.color.magnitude5; break;
+            case 6: colorResourceID = R.color.magnitude6; break;
+            case 7: colorResourceID = R.color.magnitude7; break;
+            case 8: colorResourceID = R.color.magnitude8; break;
+            case 9: colorResourceID = R.color.magnitude9; break;
+            default: colorResourceID = R.color.magnitude10plus;
+        };
+        return ContextCompat.getColor(getContext(), colorResourceID);
     }
 }
